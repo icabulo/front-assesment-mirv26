64b7240193655535f4f99680685745b7963e0bb6
Ivan <icabulo@gmail.com>
2023-04-10 22:42:43 -0500

    new Router index.jsx using createHushRouter

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,7 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createHashRouter, RouterProvider } from "react-router-dom"
 import { Home, AbouLanding, ProductDetails } from "../pages"
 
-const routerconfig = createBrowserRouter([
+const routerconfig = createHashRouter([
   {
     path: "/",
     element: <Home />,
